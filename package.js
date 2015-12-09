e306b570b6cbe4ee4e2f63abcd192ae4a5412d82
Patrick Hund <pahund@team.mobile.de>
2015-12-09 17:41:13 +0100

    code style adjustment: 4 spaces indentation, beloved double quotes
    Change-Id: I7f4bcfe0f7958aa32f4f31b5954e92cd28e249d0

--- package.js
+++ package.js
@@ -1,14 +1,14 @@
 /* eslint strict: 0, no-shadow: 0, no-unused-vars: 0, no-console: 0 */
-'use strict';
+"use strict";
 
-const os = require('os');
-const webpack = require('webpack');
-const cfg = require('./webpack.config.production.js');
-const packager = require('electron-packager');
-const del = require('del');
-const exec = require('child_process').exec;
-const argv = require('minimist')(process.argv.slice(2));
-const pkg = require('./package.json');
+const os = require("os");
+const webpack = require("webpack");
+const cfg = require("./webpack.config.production.js");
+const packager = require("electron-packager");
+const del = require("del");
+const exec = require("child_process").exec;
+const argv = require("minimist")(process.argv.slice(2));
+const pkg = require("./package.json");
 const devDeps = Object.keys(pkg.devDependencies);
 
 const appName = argv.name || argv.n || pkg.productName;
@@ -17,34 +17,34 @@ const shouldBuildAll = argv.all || false;
 
 
 const DEFAULT_OPTS = {
-  dir: './',
-  name: appName,
-  asar: shouldUseAsar,
-  ignore: [
-    '/test($|/)',
-    '/tools($|/)',
-    '/release($|/)'
-  ].concat(devDeps.map(name => `/node_modules/${name}($|/)`))
+    dir: "./",
+    name: appName,
+    asar: shouldUseAsar,
+    ignore: [
+        "/test($|/)",
+        "/tools($|/)",
+        "/release($|/)"
+    ].concat(devDeps.map(name => `/node_modules/${name}($|/)`))
 };
 
-const icon = argv.icon || argv.i || 'app/app.icns';
+const icon = argv.icon || argv.i || "app/app.icns";
 
 if (icon) {
-  DEFAULT_OPTS.icon = icon;
+    DEFAULT_OPTS.icon = icon;
 }
 
 const version = argv.version || argv.v;
 
 if (version) {
-  DEFAULT_OPTS.version = version;
-  startPack();
+    DEFAULT_OPTS.version = version;
+    startPack();
 } else {
   // use the same version as the currently-installed electron-prebuilt
-  exec('npm list electron-prebuilt', (err, stdout) => {
+  exec("npm list electron-prebuilt", (err, stdout) => {
     if (err) {
-      DEFAULT_OPTS.version = '0.36.0';
+      DEFAULT_OPTS.version = "0.36.0";
     } else {
-      DEFAULT_OPTS.version = stdout.split('electron-prebuilt@')[1].replace(/\s/g, '');
+      DEFAULT_OPTS.version = stdout.split("electron-prebuilt@")[1].replace(/\s/g, "");
     }
 
     startPack();
@@ -53,50 +53,50 @@ if (version) {
 
 
 function startPack() {
-  console.log('start pack...');
-  webpack(cfg, (err, stats) => {
-    if (err) return console.error(err);
-    del('release')
-    .then(paths => {
-      if (shouldBuildAll) {
-        // build for all platforms
-        const archs = ['ia32', 'x64'];
-        const platforms = ['linux', 'win32', 'darwin'];
+    console.log("start pack...");
+    webpack(cfg, (err, stats) => {
+        if (err) return console.error(err);
+        del("release")
+            .then(paths => {
+                if (shouldBuildAll) {
+                    // build for all platforms
+                    const archs = ["ia32", "x64"];
+                    const platforms = ["linux", "win32", "darwin"];
 
-        platforms.forEach(plat => {
-          archs.forEach(arch => {
-            pack(plat, arch, log(plat, arch));
-          });
-        });
-      } else {
-        // build for current platform only
-        pack(os.platform(), os.arch(), log(os.platform(), os.arch()));
-      }
-    })
-    .catch(err => {
-      console.error(err);
+                    platforms.forEach(plat => {
+                        archs.forEach(arch => {
+                            pack(plat, arch, log(plat, arch));
+                        });
+                    });
+                } else {
+                    // build for current platform only
+                    pack(os.platform(), os.arch(), log(os.platform(), os.arch()));
+                }
+            })
+            .catch(err => {
+                console.error(err);
+            });
     });
-  });
 }
 
 function pack(plat, arch, cb) {
-  // there is no darwin ia32 electron
-  if (plat === 'darwin' && arch === 'ia32') return;
+    // there is no darwin ia32 electron
+    if (plat === "darwin" && arch === "ia32") return;
 
-  const opts = Object.assign({}, DEFAULT_OPTS, {
-    platform: plat,
-    arch: arch,
-    prune: true,
-    out: `release/${plat}-${arch}`
-  });
+    const opts = Object.assign({}, DEFAULT_OPTS, {
+        platform: plat,
+        arch: arch,
+        prune: true,
+        out: `release/${plat}-${arch}`
+    });
 
-  packager(opts, cb);
+    packager(opts, cb);
 }
 
 
 function log(plat, arch) {
-  return (err, filepath) => {
-    if (err) return console.error(err);
-    console.log(`${plat}-${arch} finished!`);
-  };
+    return (err, filepath) => {
+        if (err) return console.error(err);
+        console.log(`${plat}-${arch} finished!`);
+    };
 }
