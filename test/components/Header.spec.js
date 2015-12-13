8e9e45ce72029ea086b974d6fc0ea992dc12958b
Patrick Hund <pahund@team.mobile.de>
2015-12-13 10:57:18 +0100

    tests for journal actions and header component
    Change-Id: Ibb207f846052a0c9fee329bdd4a6c994185d1942

---
+++ test/components/Header.spec.js
@@ -0,0 +1,56 @@
+/* eslint no-unused-expressions: 0 */
+import { expect } from "chai";
+import React from "react";
+import { Link } from "react-router";
+import {
+    renderIntoDocument,
+    scryRenderedComponentsWithType,
+    scryRenderedDOMComponentsWithTag
+} from "react-addons-test-utils";
+import Header from "../../app/components/Header";
+
+
+function setup(section = "home") {
+    const component = renderIntoDocument(<Header section={section} />);
+    return {
+        component: component,
+        links: scryRenderedComponentsWithType(component, Link),
+        listItems: scryRenderedDOMComponentsWithTag(component, "li")
+    };
+}
+
+describe("[components/Header]", () => {
+    describe("When I render the header for section “home”", () => {
+        const { links, listItems } = setup();
+        describe("the first link on the navigation", () => {
+            it("should link to route “/”", () =>
+                expect(links[0].props.to).to.equal("/"));
+            it("should have id “nav-to-list”", () =>
+                expect(links[0].props.id).to.equal("nav-to-list"));
+        });
+        describe("the secong link on the navigation", () => {
+            it("should link to route “/counter”", () =>
+                expect(links[1].props.to).to.equal("/counter"));
+            it("should have id “nav-to-counter”", () =>
+                expect(links[1].props.id).to.equal("nav-to-counter"));
+        });
+        describe("the list of navigation items", () =>
+            it("should have two items", () =>
+                expect(listItems.length).to.equal(2)));
+        describe("the first navigation item", () =>
+            it("should have class “active”", () =>
+                expect(listItems[0].className).to.equal("active")));
+        describe("the second navigation item", () =>
+            it("should not have class “active”", () =>
+                expect(listItems[1].className).to.equal("")));
+    });
+    describe("When I render the header for section “counter”", () => {
+        const { listItems } = setup("counter");
+        describe("the first navigation item", () =>
+            it("should not have class “active”", () =>
+                expect(listItems[0].className).to.equal("")));
+        describe("the second navigation item", () =>
+            it("should have class “active”", () =>
+                expect(listItems[1].className).to.equal("active")));
+    });
+});
